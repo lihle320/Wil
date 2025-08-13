a6d2b54045199d22570f566834fa2649c1275b83
mpho <ST10408316@vcconnect.edu.za>
2025-08-13 23:14:51 +0200

    create note + enter note pages incomplete

--- WILPROJECT/app/build.gradle.kts
+++ WILPROJECT/app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
